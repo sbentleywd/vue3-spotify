0c3039588e1aa08e8d9eeeea548f0f286ea1b842
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2023-02-25 14:33:28 +0000

    Remove default

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import './assets/main.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vuetify)
 
 app.mount('#app')
