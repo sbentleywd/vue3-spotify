59223628843fa0e773393845ee096792db0cc109
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2023-03-19 17:35:54 +0000

    Get to tracks

--- src/components/Selection/Tracks.vue
+++ src/components/Selection/Tracks.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from '../../stores/userStore'
+import { computed } from '@vue/reactivity'
+const userStore = useUserStore()
+const topTracks = computed(() => userStore.topTracks)
+
+userStore.getTopTracks()
+</script>
 
 <template>
   <h2>Tracks</h2>
+  <p v-for="(track, index) in topTracks" :key="index">{{ track.name }}</p>
 </template>
 
 <style scoped></style>
