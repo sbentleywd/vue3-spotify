afe5e9b9a98d602ca06a3efc1aa2aaf36e0b18b5
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2023-03-25 15:02:42 +0000

    Setup selecting artists

--- src/components/Selection/ArtistItem.vue
+++ src/components/Selection/ArtistItem.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
+import { useSelectionStore } from '@/stores/selectionStore'
 import type { artist } from '@/types'
 
-const props = defineProps<{ itemData: artist }>()
+const selectionStore = useSelectionStore()
+const props = defineProps<{ itemData: artist; selected: boolean; disabled: boolean }>()
 </script>
 
 <template>
-  <v-list-item :prepend-avatar="props.itemData.images[2].url">{{
-    props.itemData.name
-  }}</v-list-item>
+  <v-list-item
+    :prepend-avatar="props.itemData.images[2].url"
+    @click="selectionStore.toggleArtistSelection(itemData)"
+    :disabled="props.disabled"
+    >{{ props.itemData.name }}
+    <template v-slot:append>
+      <v-icon color="secondary">{{ props.selected ? 'mdi-check' : '' }}</v-icon>
+    </template>
+  </v-list-item>
 </template>
--- src/components/Selection/Artists.vue
+++ src/components/Selection/Artists.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 import { useUserStore } from '../../stores/userStore'
+import { useSelectionStore } from '@/stores/selectionStore'
 import { computed } from '@vue/reactivity'
 import ArtistItem from '@/components/Selection/ArtistItem.vue'
 const userStore = useUserStore()
+const selectionStore = useSelectionStore()
 const topArtists = computed(() => userStore.topArtists)
+const selectedArtistIds = computed(() => selectionStore.getSelectedArtistIds)
 
 userStore.getTopArtists()
 </script>
 
 <template>
   <v-list class="rounded-lg">
-    <ArtistItem v-for="(artist, index) in topArtists" :key="index" :item-data="artist" />
+    <ArtistItem
+      v-for="(artist, index) in topArtists"
+      :key="index"
+      :item-data="artist"
+      :selected="selectedArtistIds.includes(artist.id)"
+      :disabled="selectedArtistIds.length > 4 && !selectedArtistIds.includes(artist.id)"
+    />
   </v-list>
 </template>
 
