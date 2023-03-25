a5d33e07d183a651438503fbd2bac477549d2b49
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2023-03-25 13:26:29 +0000

    Set up artist list item

---
+++ src/components/Selection/ArtistItem.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { artist } from '@/types'
+
+const props = defineProps<{ itemData: artist }>()
+</script>
+
+<template>
+  <v-list-item :prepend-avatar="props.itemData.images[2].url">{{
+    props.itemData.name
+  }}</v-list-item>
+</template>
--- src/components/Selection/Artists.vue
+++ src/components/Selection/Artists.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useUserStore } from '../../stores/userStore'
 import { computed } from '@vue/reactivity'
+import ArtistItem from '@/components/Selection/ArtistItem.vue'
 const userStore = useUserStore()
 const topArtists = computed(() => userStore.topArtists)
 
@@ -8,8 +9,9 @@ userStore.getTopArtists()
 </script>
 
 <template>
-  <h2>Artists</h2>
-  <p v-for="(artist, index) in topArtists" :key="index">{{ artist.name }}</p>
+  <v-list class="rounded-lg">
+    <ArtistItem v-for="(artist, index) in topArtists" :key="index" :item-data="artist" />
+  </v-list>
 </template>
 
 <style scoped></style>
