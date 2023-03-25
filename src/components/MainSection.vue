a2e09958d54682272cd38d0d8bae91e5329321bb
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2023-03-25 13:26:04 +0000

    Update columns

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 import SeedSelection from './Selection/SeedSelection.vue'
-import Seeds from './Seeds.vue'
 import Results from './Results.vue'
 </script>
 
 <template>
   <v-row>
-    <v-col lg="4" md="6" sm="12" class="text-center">
+    <v-col cols="6" sm="6" xs="12" class="text-center">
       <SeedSelection />
     </v-col>
-    <v-col lg="4" md="6" sm="12" class="text-center">
-      <Seeds />
-    </v-col>
-    <v-col lg="4" md="6" sm="12" class="text-center">
+    <v-col cols="6" sm="6" xs="12" class="text-center">
       <Results />
     </v-col>
   </v-row>
