190833ef9a85f5e8e6b70f020fd5a5fbb6d1109f
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2023-04-07 12:25:06 +0100

    Setup basic controls

--- src/components/Player/Player.vue
+++ src/components/Player/Player.vue
@@ -1,18 +1,49 @@
 <template>
-  <h2>Player</h2>
-  <p>Script loaded: {{ scriptLoaded }}</p>
+  <v-row v-if="currentTrack">
+    <v-col cols="2">
+      <v-avatar>
+        <v-img :src="trackImageUrl" alt="John"></v-img>
+      </v-avatar>
+    </v-col>
+    <v-col col="10">
+      <v-row>
+        <v-col cols="12" md="6" xs="12"
+          ><span class="text-truncate px-2">{{
+            currentTrack.name + ' - ' + artistName
+          }}</span></v-col
+        >
+        <v-col cols="12" md="6" xs="12">
+          <v-icon @click="spotifyPlayer.previousTrack()" icon="mdi-skip-previous" />
+          <v-icon v-if="!playerState.paused" @click="spotifyPlayer.togglePlay()" icon="mdi-pause" />
+          <v-icon v-else @click="spotifyPlayer.togglePlay()" icon="mdi-play" />
+          <v-icon @click="spotifyPlayer.nextTrack()" icon="mdi-skip-next" /> </v-col
+      ></v-row>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useAuthStore } from '@/stores/authStore'
 import { usePlayerStore } from '@/stores/playerStore'
+import type { track } from '@/types'
+import { formatArtists } from '@/helpers'
 
 const authStore = useAuthStore()
 const playerStore = usePlayerStore()
 
 const scriptLoaded = ref(false)
 const spotifyPlayer = ref<any>(null)
+const playerState = ref<null | any>(null)
+const currentTrack = computed<track | null>(
+  () => playerState?.value?.track_window.current_track || null
+)
+const trackImageUrl = computed(
+  () => currentTrack.value?.album.images.find((img) => img.height === 64)?.url || ''
+)
+const artistName = computed(() =>
+  currentTrack.value ? formatArtists(currentTrack.value.artists) : ''
+)
 
 const loadScript = async () => {
   const script = document.createElement('script')
@@ -43,6 +74,11 @@ const loadScript = async () => {
     // @ts-ignore
     player.addListener('not_ready', ({ device_id }) => {
       console.log('Device ID has gone offline', device_id)
+    })
+
+    // @ts-ignore
+    player.addListener('player_state_changed', (state) => {
+      playerState.value = state
     })
 
     player.connect()
